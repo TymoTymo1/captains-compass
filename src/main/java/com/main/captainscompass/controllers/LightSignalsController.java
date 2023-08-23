6b5bddd7c19ebd64d59cf77f0c158f13fdd1b82d
TymoTymo1 <79015481+TymoTymo1@users.noreply.github.com>
2023-08-23 10:37:10 +0200

    Added quiz content

--- src/main/java/com/main/captainscompass/controllers/LightSignalsController.java
+++ src/main/java/com/main/captainscompass/controllers/LightSignalsController.java
@@ -3,13 +3,23 @@ package com.main.captainscompass.controllers;
 import com.main.captainscompass.scenes.Scenes;
 import com.main.captainscompass.scenes.Utils;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class LightSignalsController {
+public class LightSignalsController implements Initializable {
+
+    @FXML
+    ImageView quizImage;
+    @FXML
+    Label quizLabel;
 
     @FXML
     void goBack(MouseEvent event) throws IOException {
@@ -17,4 +27,8 @@ public class LightSignalsController {
         Utils.changeScene(Scenes.MAIN_SCENE, activeStage);
     }
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
